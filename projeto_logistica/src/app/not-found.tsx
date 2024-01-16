4e957c8baaa22b87d2d823550e6b694ee95a90fd
Amanda Laureano <ens-amandacruz@uniguacu.edu.br>
2024-01-16 14:21:32 -0300

    Consertado erros de build

--- projeto_logistica/src/app/not-found.tsx
+++ projeto_logistica/src/app/not-found.tsx
@@ -1,42 +1,16 @@
 'use client'
 import { PiArrowBendDownLeftBold } from "react-icons/pi"
-import animationData from '../app/Animation - 1704829687849.json'
-import Lottie from 'react-lottie';
-import { useEffect, useState } from "react";
 import { useRouter } from 'next/navigation';
 
 export default function NotFound(){
 
     const router = useRouter()
 
-    const [isClient, setIsClient] = useState(false);
-
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
-
-
-    const defaultOptions = {
-        loop: true,
-        autoplay: true, 
-        animationData: animationData,
-        rendererSettings: {
-        preserveAspectRatio: 'xMidYMid slice'
-        }
-    }
-
     return(
         <main className="flex h-screen w-screen bg-black justify-center place-content-center">
             <div className="place-self-center">
-                <div className="w-full flex justify-center place-content-center">
-                {isClient && (
-                        <div style={{ width: '80%', maxWidth: '450px', height: 'auto' }}>
-                            <Lottie options={defaultOptions} />
-                        </div>
-                    )}
-                </div>
                 <div className="mt-8 mx-5">
-                    <h1 className="text-center text-white font-bold text-xl sm:text-2xl xl:text-3xl tracking-wide">Página não encontrada!</h1>
+                    <h1 className="text-center text-white font-bold text-2xl sm:text-3xl xl:text-5xl tracking-wide">Página não encontrada!</h1>
                 </div>
                 <div className="mt-8 mx-5">
                     <button onClick={() => router.push('/')} className="h-full w-full rounded-md bg-black-light shadow-sm shadow-green-simple hover:scale-95 transition-all duration-200">
